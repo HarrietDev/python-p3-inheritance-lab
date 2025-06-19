5c31a19a58f604e40ecae6d645459b0eb64e9855
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-06-20 01:42:34 +0300

    practise on inheritances and use of super function

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,33 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name,knowledge=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge if knowledge else [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 
-    def teach(self):
-        pass
+
+    def teach(self,topic = None):
+        if not self.knowledge:
+            return None
+        if topic:
+            self.knowledge.append(topic)
+            return topic
+        return random.choice(self.knowledge)
+        
+my_teacher = Teacher("Nancy","Johns",knowledge=["pipenv install pipenv shell",
+    "pytest -x flag to fail fast"])
+print(my_teacher.__dict__)
+print(my_teacher.knowledge)
+print(my_teacher.teach("JavaScript async web request"))
+print(my_teacher.teach())
+
+    
